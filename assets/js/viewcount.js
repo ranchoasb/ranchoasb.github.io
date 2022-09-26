58c78d19d5d607b6ca9c5673d4ea03c8e1f96a1d
Timanana <72578464+Timanana@users.noreply.github.com>
2022-09-25 17:24:51 -0700

    Use original URL for Google not to get mad

--- assets/js/viewcount.js
+++ assets/js/viewcount.js
@@ -1,3 +1,3 @@
 addEventListener("load", () => {
-  if (!document.referrer.startsWith("https://ranchoasb.org")) fetch("https://script.google.com/macros/s/AKfycbx990a0bZ2hgtHk5aJyBbEsIYE2a5Nw67bAPuCBItYVsKiBDAgXGMBEy_PP2Ky_joZk/exec/increment", {mode: "no-cors"});
+  if (!document.referrer.startsWith("https://ranchoasb.org")) fetch("https://script.google.com/macros/s/AKfycbx990a0bZ2hgtHk5aJyBbEsIYE2a5Nw67bAPuCBItYVsKiBDAgXGMBEy_PP2Ky_joZk/exec", {mode: "no-cors"});
 });
