782402e5274cfef8c1154db098af8d8a5344f561
Timanana <72578464+Timanana@users.noreply.github.com>
2022-09-25 17:17:02 -0700

    Only update view count if visitor came from different site

--- assets/js/viewcount.js
+++ assets/js/viewcount.js
@@ -1,1 +1,3 @@
-addEventListener("load", () => {fetch("https://script.google.com/macros/s/AKfycbx990a0bZ2hgtHk5aJyBbEsIYE2a5Nw67bAPuCBItYVsKiBDAgXGMBEy_PP2Ky_joZk/exec/increment", {mode: "no-cors"})});
+addEventListener("load", () => {
+  if (!document.referrer.startsWith("https://ranchoasb.org")) fetch("https://script.google.com/macros/s/AKfycbx990a0bZ2hgtHk5aJyBbEsIYE2a5Nw67bAPuCBItYVsKiBDAgXGMBEy_PP2Ky_joZk/exec/increment", {mode: "no-cors"});
+});
