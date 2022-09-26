f3978805f437b1bb8ad9f7dd12ad99ab50cc6f7d
Timanana <72578464+Timanana@users.noreply.github.com>
2022-09-25 17:09:04 -0700

    Wrong URL used for view count tracking

--- assets/js/viewcount.js
+++ assets/js/viewcount.js
@@ -1,1 +1,1 @@
-addEventListener("load", () => {fetch("https://script.google.com/macros/s/AKfycbx990a0bZ2hgtHk5aJyBbEsIYE2a5Nw67bAPuCBItYVsKiBDAgXGMBEy_PP2Ky_joZk/exec", {mode: "no-cors"})});
+addEventListener("load", () => {fetch("https://script.google.com/macros/s/AKfycbx990a0bZ2hgtHk5aJyBbEsIYE2a5Nw67bAPuCBItYVsKiBDAgXGMBEy_PP2Ky_joZk/exec/increment", {mode: "no-cors"})});
