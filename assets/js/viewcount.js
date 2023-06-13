fab4178f03aaf0bf44eb9e6d6b46997e9bc3a9e5
Timanana <72578464+Timanana@users.noreply.github.com>
2023-06-13 09:46:08 -0700

    Change output location

--- assets/js/viewcount.js
+++ assets/js/viewcount.js
@@ -1,3 +1,3 @@
 addEventListener("load", () => {
-  if (!document.referrer.startsWith("https://ranchoasb.org")) fetch("https://script.google.com/macros/s/AKfycbx990a0bZ2hgtHk5aJyBbEsIYE2a5Nw67bAPuCBItYVsKiBDAgXGMBEy_PP2Ky_joZk/exec", {mode: "no-cors"});
+  if (!document.referrer.startsWith("https://ranchoasb.org")) fetch("https://script.google.com/macros/s/AKfycbxPzXSiO8MrHHLaSm0_YBG4ubiokddr3M8WLICqIH8fDfcYCks2KFBfANW0HnU2LnY5/exec?query=ping", {mode: "no-cors"});
 });
