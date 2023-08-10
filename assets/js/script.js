f5b05df11af9107fef044491f32c7b6e1fc7a562
Alvin Chen <69829040+Red256@users.noreply.github.com>
2023-08-10 13:22:14 -0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,13 +71,6 @@ window.onload = () => {
     }
     return result;
   }
-  
-  function show(date) {
-    schedule.textContent = get_schedule_for_day(date);
-    
-    let tomorrow_date = new Date(new Date().getTime() + 86400000);
-    tomorrow.textContent = get_schedule_for_day(tomorrow_date);
-  }
 
   let data;
   let datalen;
