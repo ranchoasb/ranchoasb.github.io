56952f06a44cdeaba8cc5da4dd7b41a626b3442f
Alvin Chen <69829040+Red256@users.noreply.github.com>
2023-08-10 21:26:13 -0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,7 +39,7 @@ window.onload = () => {
   }
   fetch("https://script.google.com/macros/s/AKfycbylp7XIw-zKcFtz1tOjPAI9_sR-I3PYyjP1bMXWTyrio3IYEMBNxeg2XT_1X9DzU_4H/exec?query=announcements").then(e=>e.text()).then(response => {
     for(let i=0;i<3;i++){
-      announcements.innerHTML+=`<u><a href="${response.slice(response.indexOf("https://docs.google.com/presentation/d/"),response.indexOf(">",response.indexOf("https://docs.google.com/presentation/d/"))-1)}">Announcements for ${response.slice(response.indexOf("data-date=")+11,response.indexOf('/2023"><'))}</a></u><br>`
+      announcements.innerHTML+=`<u><a href="${response.slice(response.indexOf("https://docs.google.com/presentation/d/"),response.indexOf(">",response.indexOf("https://docs.google.com/presentation/d/"))-1)}">${response.slice(response.indexOf("data-date=")+11,response.indexOf('/2023"><'))} Announcements</a></u><br>`
       response = response.replace("https://", "");
       response = response.replace("sharing", "");
       response = response.replace("data-date", "");
