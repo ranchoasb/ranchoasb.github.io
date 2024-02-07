701f8a1901836f657ae711ca45d4aad53495d670
Alvin Chen <69829040+Red256@users.noreply.github.com>
2024-02-07 00:07:21 -0800

    Fixed bug with advisement slides section on main page
    Adv slides only showed if year was 2023

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,15 +37,17 @@ window.onload = () => {
       loadmorebutton.remove();
     }
   }
+
   fetch("https://script.google.com/macros/s/AKfycbylp7XIw-zKcFtz1tOjPAI9_sR-I3PYyjP1bMXWTyrio3IYEMBNxeg2XT_1X9DzU_4H/exec?query=announcements").then(e=>e.text()).then(response => {
     try{
+      let schoolYear = new Date().getMonth() >= 7 ? new Date().getFullYear() : new Date().getFullYear() - 1;
       for(let i=0;i<Math.min(3,response.match(/Advisement Announcements for /g).length);i++) {
-        announcements.innerHTML+=`<u><a href="${response.slice(response.indexOf("https://docs.google.com/presentation/d/"),response.indexOf(">",response.indexOf("https://docs.google.com/presentation/d/"))-1)}">${response.slice(response.indexOf("data-date=")+11,response.indexOf('/2023"><'))} Announcements</a></u><br>`
+        announcements.innerHTML+=`<u><a href="${response.slice(response.indexOf("https://docs.google.com/presentation/d/"),response.indexOf(">",response.indexOf("https://docs.google.com/presentation/d/"))-1)}">${response.slice(response.indexOf("data-date=")+11,response.indexOf('/"+schoolYear><'))} Announcements</a></u><br>`
         response = response.replace("https://", "");
         response = response.replace("sharing", "");
         response = response.replace("data-date", "");
-        response = response.replace("/2023", "");
-        response = response.replace("/2023", "");
+        response = response.replace("/" + schoolYear, "");
+        response = response.replace("/" + schoolYear, "");
       }
       announcements.innerHTML+='<br>Older announcements can be found <a href="announcements">here.</a>';
     }
