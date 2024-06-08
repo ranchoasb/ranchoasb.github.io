146ab444ba9357aba6c809dbdedb3fa1e8c0bae2
Carl Liu <142947035+OrigamiKoala@users.noreply.github.com>
2024-06-08 15:53:16 -0700

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,20 +1,20 @@
-var login = document.cookie.split(";").find((row) => row.startsWith("login=")).split("=")[1];
+var login = localStorage.getItem("login");
 if (login) {
-  var iss = document.cookie.split(";").find((row) => row.startsWith("iss=")).split("=")[1];
-  var nbf = document.cookie.split(";").find((row) => row.startsWith("nbf=")).split("=")[1];
-  var sub = document.cookie.split(";").find((row) => row.startsWith("sub=")).split("=")[1];
-  var hd = document.cookie.split(";").find((row) => row.startsWith("hd=")).split("=")[1];
-  var email = document.cookie.split(";").find((row) => row.startsWith("email=")).split("=")[1];
-  var email_verified = document.cookie.split(";").find((row) => row.startsWith("email_verified=")).split("=")[1];
-  var azp = document.cookie.split(";").find((row) => row.startsWith("azp=")).split("=")[1];
-  var name = document.cookie.split(";").find((row) => row.startsWith("name=")).split("=")[1];
-  var picture = document.cookie.split(";").find((row) => row.startsWith("picture=")).split("=")[1];
-  var given_name = document.cookie.split(";").find((row) => row.startsWith("given_name=")).split("=")[1];
-  var family_name = document.cookie.split(";").find((row) => row.startsWith("family_name=")).split("=")[1];
-  var iat = document.cookie.split(";").find((row) => row.startsWith("iat=")).split("=")[1];
-  var exp = document.cookie.split(";").find((row) => row.startsWith("exp=")).split("=")[1];
-  var jti = document.cookie.split(";").find((row) => row.startsWith("jti=")).split("=")[1];
-  var aud = document.cookie.split(";").find((row) => row.startsWith("aud=")).split("=")[1];
+  var iss = localStorage.getItem("login");
+  var nbf = localStorage.getItem("login");
+  var sub = localStorage.getItem("login");
+  var hd = localStorage.getItem("login");
+  var email = localStorage.getItem("login");
+  var email_verified = localStorage.getItem("login");
+  var azp = localStorage.getItem("login");
+  var name = localStorage.getItem("login");
+  var picture = localStorage.getItem("login");
+  var given_name = localStorage.getItem("login");
+  var family_name = localStorage.getItem("login");
+  var iat = localStorage.getItem("login");
+  var exp = localStorage.getItem("login");
+  var jti = localStorage.getItem("login");
+  var aud = localStorage.getItem("login");
 }
 else {
   var iss = null;
