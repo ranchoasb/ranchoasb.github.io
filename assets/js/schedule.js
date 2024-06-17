82d07612ea62095e9a29d83cd99a7f8faf27d5a6
Carl Liu <142947035+OrigamiKoala@users.noreply.github.com>
2024-06-16 17:41:16 -0700

    Update schedule.js

--- assets/js/schedule.js
+++ assets/js/schedule.js
@@ -38,7 +38,7 @@ fetch("https://script.google.com/macros/s/AKfycbzQD0q5-QB-VX809oPY-7pUqMStIb0iuR
                 const enddate = currentdate.split("/");
               }
               else {
-                const enddate = [0, 0, 0000];
+                const enddate = ["0", "0", "0000"];
               }
               info.push([rows.occasion, startdate[0], startdate[1], startdate[2], enddate[0], enddate[2], enddate[3], rows.starttime, rows.endtime, rows.periodlength, rows.school, rows.additional]);
       }
