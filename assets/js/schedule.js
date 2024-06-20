8caddfefd08a63d7ab492787a00e2d5f9a8558bb
Carl Liu <142947035+OrigamiKoala@users.noreply.github.com>
2024-06-19 18:22:40 -0700

    Update schedule.js

--- assets/js/schedule.js
+++ assets/js/schedule.js
@@ -1,5 +1,5 @@
 const info = [];
-fetch("https://script.google.com/macros/s/AKfycbxwUGn5tktJImKvb6GtZ7ylzMkQUbI5o7Jofv6xOi8WnlOhFBgS91zuLfPRXeTIGpGe/exec?query=schedule", {mode: "no-cors"}).then(response => response.json()).then(data => {
+fetch("https://script.google.com/macros/s/AKfycby1YhxB5_-YF5uSiDHP5PTsurBwG9ReYlxsPhYpVlit0WzZteoBwrn-UOaHhkRi60Oj/exec?query=schedule", {mode: "no-cors"}).then(response => response.json()).then(data => {
       let rows = data.slice(0, data.length);
       for (let index in rows) {
               let row = rows[index];
