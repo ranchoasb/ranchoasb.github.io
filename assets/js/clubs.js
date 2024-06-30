135bd78ffabdc959ebe34e053122ff927c39cc69
Carl Liu <142947035+OrigamiKoala@users.noreply.github.com>
2024-06-30 12:47:38 -0700

    Update clubs.js

--- assets/js/clubs.js
+++ assets/js/clubs.js
@@ -18,12 +18,13 @@ fetch("https://script.google.com/macros/s/AKfycbx5QXiwFiOzIKM_8KrFYAERtcrkYjJ3wu
 }).then(a => {
   let current = document.getElementById("clubstext");
   let result = `<p>On specific days, certain lunch clubs—student-led activities sponsored by a teacher—meet in a classroom and partake in engaging and community-building activities. Find a teacher to sponsor your club and talk to our principal, Mrs. Snowden, if you want to form your own!</p><h4>Clubs today:</h4>`;
+  result+="<ul>";
   for (let x in info){
     let y = new Date();
     z = y.getDay();
     if (0 < z && z<6){
       if (x[3+z]=="1"){
-        result+="<ul><li><b>"+x[0]+" -"+x[2]+":</b>"+" "+x[1]+"</li>";
+        result+="<li><b>"+x[0]+" -"+x[2]+":</b>"+" "+x[1]+"</li>";
       }
     }
   }
