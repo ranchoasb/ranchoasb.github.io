d951e5725fa1ac74ca7d13200376c403899ce3e1
Carl Liu <142947035+OrigamiKoala@users.noreply.github.com>
2024-06-30 14:02:08 -0700

    Update schedule.js

--- assets/js/schedule.js
+++ assets/js/schedule.js
@@ -33,7 +33,7 @@ function dayType(month, day, dayName){
 }
 // gets schedule for any day
 function get_schedule_for_day(d){
- if (d.getTime() < 1724167200000) {
+ if (d.getTime() < 1723532400) {
     document.getElementById("schedule").textContent = "The 2024 to 2025 school year has not started yet!";
   } else if (d.getTime() > 1749207600000) {
     document.getElementById("schedule").textContent = "The 2024 to 2025 school year has concluded.";
