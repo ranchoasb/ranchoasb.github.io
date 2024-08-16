2c7d71224be1311b6beacdd2489fdb02f99c15d2
Rancho ASB <80703874+ranchoasb@users.noreply.github.com>
2024-08-16 10:55:52 -0700

    Update loginfunctions.js

--- assets/js/loginfunctions.js
+++ assets/js/loginfunctions.js
@@ -36,7 +36,7 @@ function handleCredentialResponse(response){
     console.log("Image URL: " + info.picture);
     console.log("Email: " + info.email);
     console.log("hd: " + info.hd);
-    fetch("https://script.google.com/macros/s/AKfycbxW4SCusqPSHTb-6BXTMmaKchbpQf6dd79CbB8spT_aG7ax2KTJfgYTFDAVdJ8xLi1Y/exec?query=retrieveEmail", {mode: "no-cors"}).then(response => JSON.parse(String(response))).then(data => {
+    fetch("https://script.google.com/macros/s/AKfycbxW4SCusqPSHTb-6BXTMmaKchbpQf6dd79CbB8spT_aG7ax2KTJfgYTFDAVdJ8xLi1Y/exec?query=retrieveEmail", {mode: "no-cors"}).then(response => JSON.parse('${response}')).then(data => {
         let x = 0;
         let isiusd = 0;
         if (info.hd=="iusd.org"){
