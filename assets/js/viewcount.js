540e02dc5ef2bf1169f2e907ade070870f2f9473
Carl Liu <142947035+OrigamiKoala@users.noreply.github.com>
2024-08-22 19:10:37 -0700

    Update viewcount.js

--- assets/js/viewcount.js
+++ assets/js/viewcount.js
@@ -1,3 +1,8 @@
 addEventListener("load", () => {
-  if (!document.referrer.startsWith("https://ranchoasb.org") && localStorage.getItem("email")!="kingboobowser2011@gmail.com") fetch("https://script.google.com/macros/s/AKfycby9qWGns9f47zJmJnyE9gNYCy-DtuqcsE68ClRIiaCGstEuBAQZgdGUstkqCFeFGMFf/exec?query=ping", {mode: "no-cors"});
+  if (!document.referrer.startsWith("https://ranchoasb.org") && localStorage.getItem("email")!="kingboobowser2011@gmail.com") {
+    let timestamp = new Date();
+    let month = timestamp.getMonth() + 1;
+    let time = timestamp.getHours().toString() + ":" + timestamp.getMinutes().toString() + ":" + timestamp.getSeconds().toString() + ", " + month.toString() + "/" + timestamp.getDate().toString() + "/" + timestamp.getFullYear().toString();
+    fetch("https://script.google.com/macros/s/AKfycby9qWGns9f47zJmJnyE9gNYCy-DtuqcsE68ClRIiaCGstEuBAQZgdGUstkqCFeFGMFf/exec?query=ping&type=View&time="+time);
+  }
 });
