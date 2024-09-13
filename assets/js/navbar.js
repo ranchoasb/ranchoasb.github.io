793310572bfc53ba442def3dd0da505f6491e02c
Carl Liu <142947035+OrigamiKoala@users.noreply.github.com>
2024-09-12 21:00:59 -0700

    Update navbar.js

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,1 +1,1 @@
-fetch("https://script.google.com/macros/s/AKfycby9qWGns9f47zJmJnyE9gNYCy-DtuqcsE68ClRIiaCGstEuBAQZgdGUstkqCFeFGMFf/exec?query=navbar").then(e=>e.text()).then((e=>{navbarhtml.outerHTML=e}));
+fetch("https://script.google.com/macros/s/AKfycbxdD6RYVdcFZkvs7ONSet3sjezfT1ZlXVQVc4Sw8of4vnH_e3i4Ko-q8tyZBfti_z7G/exec?query=navbar").then(e=>e.text()).then((e=>{navbarhtml.outerHTML=e}));
