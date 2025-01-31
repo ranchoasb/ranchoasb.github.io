3b4d05c634cf964da0859bc6225959ae3d906f58
Carl Liu <142947035+OrigamiKoala@users.noreply.github.com>
2025-01-30 18:06:43 -0800

    Update viewcount.js

--- assets/js/viewcount.js
+++ assets/js/viewcount.js
@@ -3,6 +3,6 @@ addEventListener("load", () => {
     let timestamp = new Date();
     let month = timestamp.getMonth() + 1;
     let time = timestamp.getHours().toString() + ":" + timestamp.getMinutes().toString() + ":" + timestamp.getSeconds().toString() + ", " + month.toString() + "/" + timestamp.getDate().toString() + "/" + timestamp.getFullYear().toString();
-    fetch("https://script.google.com/macros/s/AKfycbxuZsKu9IGYFQXjS935qEsMAlwdCeEjMvJnyouQo7hTG8msvRBS_aA30702SsaCmwLv/exec?query=ping&type=View&time="+time);
+    fetch("https://script.google.com/macros/s/AKfycbxFqlnRMtz-mR8-uJAAJmrPhcdRXQ8ui4SmkdKYJq75Y23pnVRds9UKH2NrAFiyjmRP/exec?query=ping&type=View&time="+time);
   }
 });
